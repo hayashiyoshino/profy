1d087fce023de3c656cd80efa6fd93c296a96161
林佳志乃 <twinklestaryoshino@gmail.com>
2018-06-13 20:58:11 +0900

    add port

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,7 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_url_options = { host: 'ec2-54-235-206-118.compute-1.amazonaws.com' }
+  config.action_mailer.default_url_options = { host: 'ec2-54-235-206-118.compute-1.amazonaws.com', port: 5432 }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
